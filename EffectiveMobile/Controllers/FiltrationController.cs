1070cb173784ed5d99f9aab5ca9b7a1510ccc241
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2024-11-10 15:07:47 +0300

    added custom structured logging

--- EffectiveMobile/Controllers/FiltrationController.cs
+++ EffectiveMobile/Controllers/FiltrationController.cs
@@ -3,6 +3,7 @@ using EffectiveMobile.Database.Models;
 using EffectiveMobile.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Log = Serilog.Log;
 
 namespace EffectiveMobile.Controllers;
 
@@ -13,7 +14,10 @@ public class FiltrationController: ControllerBase
     private readonly ILogger<FiltrationController> _logger;
     private readonly IFiltrationService _filtrationService;
 
-    public FiltrationController(AppDbContext db, ILogger<FiltrationController> logger, IFiltrationService filtrationService)
+    public FiltrationController(
+        AppDbContext db,
+        ILogger<FiltrationController> logger,
+        IFiltrationService filtrationService)
     {
         _db = db;
         _logger = logger;
@@ -21,9 +25,20 @@ public class FiltrationController: ControllerBase
     }
 
     [HttpGet("filter-data")]
-    public async Task<IActionResult> FilterData(string district, DateTime firstDeliveryDate)
+    public async Task<IActionResult> FilterData(
+        string district,
+        DateTime firstDeliveryDate)
     {
+        using var scope1 = _logger.BeginScope(new { TimeNow1 = DateTime.UtcNow });
+        {
+            _logger.LogInformation("Random custom logging {district}, {firstDeliveryDate}", district,firstDeliveryDate);
+        }
         var filteredDeliveries = await _filtrationService.FilterDeliveries(district, firstDeliveryDate);
+
+        using var scope2 = _logger.BeginScope(new { TimeNow2 = DateTime.UtcNow });
+        {
+            _logger.LogError(new Exception("test"), "Something went wrong kek {someDate}", DateTime.UtcNow );
+        }
         
         return Ok(filteredDeliveries);
     }
